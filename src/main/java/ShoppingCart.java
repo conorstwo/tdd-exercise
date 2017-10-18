b0cb60232cab3e2f77a39e84af3ab9fb454f71c8
conorscott <conorstwo@hotmail.co.uk>
2017-10-18 19:11:44 +0100

    Improving solution by converting calculations to be functions

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -2,6 +2,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Class to represent a shopping cart and related functions
@@ -15,49 +17,48 @@ public class ShoppingCart {
     }
 
     /**
-     * Calculate the total cost of all items in the basket with discounts applied
-     * @return total cost of all items in the basket after discounts
+     * Convenience method for calling function
+     * @return - total cost of all items in basket with discounts applied
      */
     public BigDecimal calculateTotalCost() {
-        return calculateTotalCostBeforeDiscounts()
-                .subtract(calculateBuyMultipleGetOneFreeDiscount(Item.APPLE))
-                .subtract(calculateBuyMultipleGetOneFreeDiscount(Item.ORANGE));
+        return calculateTotalCost.apply(items);
     }
 
     /**
-     * Calculate the total cost of the items in the basket without discounts
-     * @return the pre discounts total cost of the items
+     * Calculate the number of the given item in the supplied list
      */
-    private BigDecimal calculateTotalCostBeforeDiscounts() {
-        return items.stream()
+    private final BiFunction<List<Item>, Item, Long> getNumberOfItem = (itemList, item) ->
+            itemList.stream()
+                    .filter(Objects::nonNull)
+                    .filter(currentItem -> item.equals(currentItem))
+                    .count();
+
+    /**
+     * Calculate the total cost of the items in the basket without discounts
+     */
+    private final Function<List<Item>, BigDecimal> calculateTotalCostBeforeDiscounts = itemList ->
+            itemList.stream()
                 .filter(Objects::nonNull)
                 .map(Item::getCost)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
 
     /**
      * Calculate the discount to be subtracted from the total cost for the given item
      * For example: apples are buy 1 get 1 free and cost 60p so the discount to be applied for 2 apples in the basket would be 60p
-     * @param item - value of {@link Item}
-     * @return the discount from the buy multiple get one free offer
      */
-    private BigDecimal calculateBuyMultipleGetOneFreeDiscount(final Item item) {
-        final long numberOfItem = getNumberOfItem(item);
+    private BiFunction<List<Item>, Item, BigDecimal> calculateBuyMultipleGetOneFreeDiscount = (itemList, item) -> {
+        final long numberOfItem = getNumberOfItem.apply(itemList, item);
         final long numberOfDiscountsToApply = Math.floorDiv(numberOfItem, item.getNumberOfItemsForBuyMultipleGetOneFreeOffer());
         return item.getCost().multiply(new BigDecimal(numberOfDiscountsToApply));
-    }
+    };
 
     /**
-     * Calculate the number of the given item in the shopping cart
-     * @param item - the item to count
-     * @return the number of the item in the cart
+     * Calculate the total cost of all items in the basket with discounts applied
      */
-    private long getNumberOfItem(final Item item) {
-        return items.stream()
-                .filter(Objects::nonNull)
-                .filter(currentItem -> item.equals(currentItem))
-                .count();
-    }
+    public final Function<List<Item>, BigDecimal> calculateTotalCost = itemList ->
+            calculateTotalCostBeforeDiscounts.apply(itemList)
+                    .subtract(calculateBuyMultipleGetOneFreeDiscount.apply(itemList, Item.APPLE))
+                    .subtract(calculateBuyMultipleGetOneFreeDiscount.apply(itemList, Item.ORANGE));
 
     public void addItem(final Item item) {
         items.add(item);
